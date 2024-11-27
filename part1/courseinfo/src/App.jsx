00ce187c024b307c6b9d453e3dbbf1eee3f7ceb2
oscar <denfu.oscar.dev@gmail.com>
2024-11-27 17:12:18 +0000

    Course information step 3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,28 +1,24 @@
 import { Fragment } from "react";
 const App = () => {
-  const courseinfo = {
-    name: `Half Stack application development`,
-    parts: [
-      {
-        name: `Fundamentals of React`,
-        exercises: 10,
-      },
-      {
-        name: `Using props to pass data`,
-        exercises: 7,
-      },
-      {
-        name: `State of a component`,
-        exercises: 14,
-      },
-    ],
+  const course = "Half Stack application development";
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
   };
 
   return (
     <div>
-      <Header course={courseinfo} />
-      <Content parts={courseinfo.parts} />
-      <Total parts={courseinfo.parts} />
+      <Header course={course} />
+      <Content parts={[part1, part2, part3]} />
+      <Total parts={[part1, part2, part3]} />
     </div>
   );
 };
@@ -30,7 +26,7 @@ const App = () => {
 export default App;
 
 function Header({ course }) {
-  return <h1>{course.name}</h1>;
+  return <h1>{course}</h1>;
 }
 function Content({ parts }) {
   return (
